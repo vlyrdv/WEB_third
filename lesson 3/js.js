d7ce52a2f880a0ec2e7880bea0570c3a091ed22d
vlyrdv <89237091511@mail.ru>
2022-08-23 19:31:57 +0700

    new_commmmmmit

--- lesson 3/js.js
+++ lesson 3/js.js
@@ -30,8 +30,7 @@ newCard = {
 }
 
 function assignNewCard(card) {
-    let clone = 
-    cards.push(clone)
+    cards.push(JSON.parse(JSON.stringify(card)));
 }
 
 
@@ -46,51 +45,58 @@ console.log(cards[2].content.title) // тоже будет 'test3'
 
 
 
-//N2
+N2
 
 
-// let data = [{
-//         brand: "Apple",
-//         name: "MacBook",
-//         model: "Air 13 2022",
-//         type: "Notebook",
-//         cost: "1300",
-//         color: "silver"
-//     },
-//     {
-//         brand: "Samsung",
-//         name: "YADAUN",
-//         model: "S22",
-//         type: "Smartphone",
-//         cost: "1500",
-//         color: "gray"
-//     }
-// ]
+let data = [{
+        brand: "Apple",
+        name: "MacBook",
+        model: "Air 13 2022",
+        type: "Notebook",
+        cost: "1300",
+        color: "silver"
+    },
+    {
+        brand: "Samsung",
+        name: "YADAUN",
+        model: "S22",
+        type: "Smartphone",
+        cost: "1500",
+        color: "gray"
+    }
+]
 
-// function sortCost1(a, b, decrease = false) {
-//     if (a.brand > b.brand) {
-//         return 1
-//     } else {
-//         return -1
-//     }
-// }
+function sortCost1(a, b) {
+    if (a.brand > b.brand) {
+        return 1
+    } else {
+        return -1
+    }
+}
 
-// function sortCost2(a, b, decrease = false) {
-//     if (a.cost > b.cost) {
-//         return 1
-//     } else {
-//         return -1
-//     }
-// }
+function sortCost2(a, b) {
+    if (a.cost > b.cost) {
+        return 1
+    } else {
+        return -1
+    }
+}
 
-// // function sortCost3(a, b, decrease = false) {
-// //     // Задание не понял
-// // }
+function sortCost3(a, b) {
+    let min = 1000;
+    let max = 3000;
 
-// function sortCost4(a, b, decrease = false) {
-//     if (a.color > b.color) {
-//         return 1
-//     } else {
-//         return -1
-//     }
-// }
+    function range(a) {
+        if (a.cost >= min && a.cost <= max) {
+            return a;
+        }
+    }
+}
+
+function sortCost4(a, b) {
+    if (a.color > b.color) {
+        return 1
+    } else {
+        return -1
+    }
+}
--- lesson 4/js.js
+++ lesson 4/js.js
@@ -1,6 +1,201 @@
-obj = [{
-    name: "Микроволновка",
-    title: "loremloremloremloremloremloremloremloremloremlorem",
-    price: "5700",
-    img: "images/микроволновка.webp"
-}]
+let obj = [{
+        name: "Микроволновка",
+        title: "loremloremloremloremlorem",
+        price: 5700,
+        img: "images/микроволновка.webp",
+        popularity: 5.0
+    }, {
+        name: "Холодильник",
+        title: "loremloremloremloremlorem",
+        price: 10200,
+        img: "images/микроволновка.webp",
+        popularity: 1.2
+    },
+    {
+        name: "Чайник",
+        title: "loremloremloremloremlorem   ",
+        price: 2000,
+        img: "images/микроволновка.webp",
+        popularity: 2.4
+    }
+]
+let obj2 = {}
+
+let funcs = {
+    sort_price: function (a, b) {
+        if (a.price > b.price) {
+            return 1;
+        } else {
+            return -1;
+        }
+    },
+    sort_price_rev: function (a, b) {
+        if (a.price < b.price) {
+            return 1;
+        } else {
+            return -1;
+        }
+    },
+    sort_alph: function (a, b) {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    },
+    sort_alph_rev: function (a, b) {
+        if (a.name < b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    },
+    sort_popular: function (a, b) {
+        if (a.popularity > b.popularity) {
+            return 1;
+        } else {
+            return -1;
+        }
+    },
+    sort_popular_rev: function (a, b) {
+        if (a.popularity < b.popularity) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+}
+for (i of obj) {
+    $(".row").append(
+        `<div class="col s4">
+            <div class="card">
+                <div class="card-image">
+                    <img src='${i.img}'>
+                    <span class="card-title" style="color: black;">${i.name}</span>
+                </div>
+                <div class="card-content">
+                    <p>${i.price}Р</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Купить</a>
+                </div>
+            </div>
+        </div>
+        `
+    );
+}
+
+// function sort_price(a, b) {
+//     if (a.price > b.price) {
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// }
+
+// function sort_price_rev(a, b) {
+//     if (a.price > b.price) {
+//         return -1;
+//     } else {
+//         return 1;
+//     }
+// }
+
+// function sort_alph(a, b) {
+//     if (a.name > b.name) {
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// }
+
+// function sort_alph_rev(a, b) {
+//     if (a.name > b.name) {
+//         return -1;
+//     } else {
+//         return 1;
+//     }
+// }
+
+// function sort_popular(a, b) {
+//     if (a.popularity > b.popularity) {
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// }
+
+// function sort_popular_rev(a, b) {
+//     if (a.popularity > b.popularity) {
+//         return -1;
+//     } else {
+//         return 1;
+//     }
+// }
+
+
+// function const_price(fst, scd) {
+//     let newMas = [];
+//     for (i in obj) {
+//         if (i.price >= fst && i.price <= scd) {
+//             newMas.push(i)
+//         }
+//     }
+// }
+
+// for (i of obj.sort()) {
+//     $(".row").append(
+//         `<div class="col s4">
+//             <div class="card">
+//                 <div class="card-image">
+//                     <img src='${i.img}'>
+//                     <span class="card-title" style="color: black;">${i.name}</span>
+//                 </div>
+//                 <div class="card-content">
+//                     <p>${i.price}Р</p>
+//                 </div>
+//                 <div class="card-action">
+//                     <a href="#">Купить</a>
+//                 </div>
+//             </div>
+//         </div>
+//         `
+//     );
+// }
+
+function vision(fnc, from, to) {
+    console.log(fnc, from, to)
+    for (i of obj.sort(funcs[fnc])) {
+        if (i.price >= from && i.price <= to) {
+            $(".row").append(
+                `<div class="col s4">
+            <div class="card">
+                <div class="card-image">
+                    <img src='${i.img}'>
+                    <span class="card-title" style="color: black;">${i.name}</span>
+                </div>
+                <div class="card-content">
+                    <p>${i.price}Р</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Купить</a>
+                </div>
+            </div>
+        </div>
+        `
+            );
+        }
+    }
+}
+// console.log($("#sort").val(), $("#from").val(), $("#to").val())
+// vision($("#sort").value(), $("#from").value(), $("#to").value())
+
+$("#sort").on("change", () => {
+    $(".col").remove()
+    vision($("#sort").val(), $("#from").val(), $("#to").val());
+})
+$("#pricevalue").on("click", () => {
+    $(".col").remove()
+    vision($("#sort").val(), $("#from").val(), $("#to").val());
+})
